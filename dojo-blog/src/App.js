1702d8025e1130b71b4aee46551637b1533ecd9f
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-03 17:08:46 -0300

    Dynamic Values in Templates ❄️
    How to use different types of values for templates

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 
 function App() {
+const title= 'Welcome to the new blog';
+const likes = 50;
+
+const person = { name: 'yokis' , age: 30};
+const link = "http://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1> App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p> {person.name}</p>
+
+        <p>{ 10 }</p>
+        <p>{ "Hello ninjas" }</p>
+        <p>{ [1,2,3,4] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+
+        <a href={link}>Google</a>
       </div>
     </div>
   );
