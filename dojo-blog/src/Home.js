1a432b48bfc68a5cf4aa115d821a4549fa4336dc
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-04 13:25:58 -0300

    Click Events ❄️
    Using on click for JSX, with and without parameters

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello world');
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello ', name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => {handleClickAgain('Paulo', e)}}>Click me again</button>
         </div>
      );
 }
