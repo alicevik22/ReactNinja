52034f9572dd556c7dfcd6b99b4d4cf4515ecd21
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-14 14:03:21 -0300

    Conditional Loading Message ❄️
    Put Loading logo until the server gets the data(in this case until pass 3 seconds)

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,19 +4,24 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPeding] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then((data) => {
-            setBlogs(data)
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+                setIsPeding(false);
+            });
+        }, 3000)
     }, []);
     
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All titles!!" />}
         </div>
      );
