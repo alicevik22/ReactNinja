304d341b4ad87a0c3906e01b849c6279c5a62f4a
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-06-14 16:28:10 -0300

    Making a Custom Hook ❄️
    Putting the Fetch method in other file

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,32 +1,9 @@
 import BlogList from "./BlogList";
-import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPeding] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok){
-                        throw Error('Could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPeding(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setIsPeding(false);
-                    setError(err.message);
-                })
-        }, 1000)
-    }, []);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
     
     return (
         <div className="home">
